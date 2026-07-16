1575023cff89e0b834b05bfa48f8df46f24a39f9
keerthiAppspire <keerthi@appspiresolutions.in>
2026-07-16 11:48:02 +0530

    Add dark mode toggle

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
-<script setup>
+<!-- <script setup>
 import Day_1 from './Day_1.vue'
+import MySkills from './components/MySkills.vue'
+
+const developer = 'KEERTHI'
+const role = 'Future Vue.js Engineer'
+const today = new Date().toLocaleDateString()
 </script>
 
 <template>
   <Day_1 />
+</template> -->
+
+<script setup>
+import Card from "./components/Card.vue"
+</script>
+
+<template>
+  <Card />
 </template>
+
+<!-- <script setup>
+import ProfileCard from "./components/ProfileCard.vue"
+</script>
+<template>
+  <ProfileCard />
+</template> -->
--- src/Day_1.vue
+++ src/Day_1.vue
@@ -1,8 +1,10 @@
 <script setup>
+import MySkills from "./components/MySkills.vue"
 const developer = "keerthi"
 const role = 'Intern'
 const location="hyderabad"
 const github="https://github.com/appspirelearner/vue-day-01"
+const today = new Date().toLocaleDateString()
 </script>
 
 <template>
@@ -10,24 +12,30 @@ const github="https://github.com/appspirelearner/vue-day-01"
    <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"alt="githubLog"
     width="100"
     />
-    <h1>Hello,i'm{{ developer }}</h1>
+    <h1>Hello,I'm{{ developer }}</h1>
     <h2>{{ role }}</h2>
     <p>{{ location }}</p>
     <p>
       <strong>GitHub:</strong>
       <a :href="github" target="_blank">{{ github }}</a>
     </p>
+    <MySkills />
+    <hr />
+     <footer>
+      <p>Today's Date: {{ today }}</p>
+    </footer>
   </main>
 </template>
 
 <style scoped>
 .container {
-  font-family: system-ui, sans-serif;
+  max-width: 600px;
+  margin: 40px auto;
+  padding: 30px;
   text-align: center;
-  padding: 4rem;
-}
-h1 { color: #42b883; }h1 {
-  color: #42b883;
+  background-color: lightblue;
+  border-radius: 12px;
+  font-family: Arial, sans-serif;
 }
 
 a {
@@ -41,5 +49,15 @@ a:hover {
 
 img {
   margin-bottom: 1rem;
+}
+
+
+h1 {
+  color: #42b883;
+}
+
+footer {
+  margin-top: 40px;
+  font-weight: bold;
 }
 </style>
